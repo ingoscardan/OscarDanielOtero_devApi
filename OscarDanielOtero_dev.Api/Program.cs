cb726f1040110ee27c121ffab801d77b68053495
Oscar Daniel Otero <ingoscardan@gmail.com>
2025-01-31 17:44:26 -0600

    Pipleneupdate

--- OscarDanielOtero_dev.Api/Program.cs
+++ OscarDanielOtero_dev.Api/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddFirestoreDb(builder.Configuration.GetConnectionString("FirestoreDbProjectId"));
 
 var app = builder.Build();
 
